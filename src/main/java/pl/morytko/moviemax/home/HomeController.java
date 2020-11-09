a34d9894325559a0d4b79f4f106f50b14329b7e2
AdamMorytko <adikmor@gmail.com>
2020-11-09 13:49:50 +0100

    success on using thymeleaf and displaying data.sql test movies list on home page

--- src/main/java/pl/morytko/moviemax/home/HomeController.java
+++ src/main/java/pl/morytko/moviemax/home/HomeController.java
@@ -1,13 +1,19 @@
 package pl.morytko.moviemax.home;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.morytko.moviemax.movies.MovieService;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+    private MovieService movieService;
 
     @RequestMapping
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        model.addAttribute("movies",movieService.getMovies());
         return "home";
     }
 }
